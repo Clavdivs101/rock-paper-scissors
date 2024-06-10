82fd6d7ef23e1c63b16f6d8b2215cd30fa3f5b3e
odinstudent <166247290+Clavdivs101@users.noreply.github.com>
2024-06-10 18:40:18 +0300

    human choice and computer choice seem to return the right things

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ function getComputerChoice(max) {
     max = 3;
     let rn = Math.floor(Math.random() * max);
     if(rn === 0){
-        return "rock";
+        return "Rock";
     }else if(rn === 1){
-        return "paper";
-    }else return "scissors";
+        return "Paper";
+    }else return "Scissors";
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(choice){
+    choice = window.prompt("Rock, Paper or Scissors ?");
+
+    if (choice == "rock"){
+        return "Rock";
+        alert("You chose Rock");
+    }else if(choice == "paper"){
+        return "Paper";
+        alert("You chose Paper");
+    }else if(choice == "scissors"){
+        return "Scissors";
+        alert("You chose scissors");}
+    }
+
+console.log(getComputerChoice()); 
+                                        // testing if these work
+console.log(getHumanChoice());
 
