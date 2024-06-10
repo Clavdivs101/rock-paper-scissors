c393736b68447a6a73c79ef631c6516a6621d52d
odinstudent <166247290+Clavdivs101@users.noreply.github.com>
2024-06-10 20:30:23 +0300

    It works I think

--- script.js
+++ script.js
@@ -25,44 +25,55 @@ function getHumanChoice(choice){
     }
     // console.log(getComputerChoice()); 
     // console.log(getHumanChoice());
-
-
     let humanScore = 0;
     let computerScore = 0;
+function playGame(){
+    
     function playRound(humanChoice, computerChoice){
     //humanChoice = getHumanChoice();
     //computerChoice = getComputerChoice();
     if(humanChoice === "paper" && computerChoice === "rock"){
-        alert("you won paper beats rock");
+        alert("you won the round paper beats rock");
         humanScore++;
     }else if(humanChoice === "paper" && computerChoice === "scissors"){
-        alert("you lost paper gets cut");
+        alert("you lost the round paper gets cut");
         computerScore++;
     }else if(humanChoice === "scissors" && computerChoice === "paper"){
-        alert("you won scissors beats paper");
+        alert("you won the round scissors beats paper");
         humanScore++;
     }else if(humanChoice === "scissors" && computerChoice === "rock"){
-        alert("you lost scissors get smashed");
+        alert("you lost the round scissors get smashed");
         computerScore++;
     }else if(humanChoice === "rock" && computerChoice === "scissors"){
-        alert("you won rock beats scissors");
+        alert("you won the round rock beats scissors");
         humanScore++;
     }else if(humanChoice === "rock" && computerChoice === "paper"){
-        alert("you lost rock gets foiled");
+        alert("you lost the round rock gets foiled");
         computerScore++;
     }else if(humanChoice === "paper" && computerChoice === "paper" || 
             humanChoice === "rock" && computerChoice === "rock" ||
             humanChoice === "scissors" && computerChoice === "scissors")
-            alert("That's a tie");
+            alert("The Round is a tie");
     }
-
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     playRound(humanSelection, computerSelection);
     console.log(humanScore, computerScore);
+}
+    for (let i = 1; i<=5; i++){
+    playGame();
+    }
+    if(humanScore > computerScore){
+        alert("You won the game!")
+        }else if (humanScore === computerScore){
+            alert("The game is tie!");
+        }else alert("You lost the game!")
+
+
+    //console.log(humanScore, computerScore);
 
 
                                         // testing if these work
 
     //console.log(getComputerChoice()); 
-    // console.log(getHumanChoice());
+    //console.log(getHumanChoice());
