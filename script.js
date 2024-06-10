d7b5d409a70dc5a8c96b285d46a7c9f604a0af84
odinstudent <166247290+Clavdivs101@users.noreply.github.com>
2024-06-10 19:31:22 +0300

    figured out the how to make the round in involved lots of ifs

--- script.js
+++ script.js
@@ -2,27 +2,67 @@ function getComputerChoice(max) {
     max = 3;
     let rn = Math.floor(Math.random() * max);
     if(rn === 0){
-        return "Rock";
+        return "rock";
     }else if(rn === 1){
-        return "Paper";
-    }else return "Scissors";
+        return "paper";
+    }else return "scissors";
+    console.log(getComputerChoice());
 }
 
 function getHumanChoice(choice){
     choice = window.prompt("Rock, Paper or Scissors ?");
 
-    if (choice == "rock"){
-        return "Rock";
-        alert("You chose Rock");
-    }else if(choice == "paper"){
-        return "Paper";
-        alert("You chose Paper");
-    }else if(choice == "scissors"){
-        return "Scissors";
-        alert("You chose scissors");}
+    if (choice.toLowerCase() === "rock"){
+        return "rock";
+        //alert("You chose Rock");
+    }else if(choice.toLowerCase() === "paper"){
+        return "paper";
+        //alert("You chose Paper");
+    }else if(choice.toLowerCase() === "scissors"){
+        return "scissors";
+        //alert("You chose scissors");
+    }
+    }
+    // console.log(getComputerChoice()); 
+    // console.log(getHumanChoice());
+
+
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice){
+    //humanChoice = getHumanChoice();
+    //computerChoice = getComputerChoice();
+    if(humanChoice === "paper" && computerChoice === "rock"){
+        alert("you won paper beats rock");
+        humanScore++;
+    }else if(humanChoice === "paper" && computerChoice === "scissors"){
+        alert("you lost paper gets cut");
+        computerScore++;
+    }else if(humanChoice === "scissors" && computerChoice === "paper"){
+        alert("you won scissors beats paper");
+        humanScore++;
+    }else if(humanChoice === "scissors" && computerChoice === "rock"){
+        alert("you lost scissors get smashed");
+        computerScore++;
+    }else if(humanChoice === "rock" && computerChoice === "scissors"){
+        alert("you won rock beats scissors");
+        humanScore++;
+    }else if(humanChoice === "rock" && computerChoice === "paper"){
+        alert("you lost rock gets foiled");
+        computerScore++;
+    }else if(humanChoice === "paper" && computerChoice === "paper" || 
+            humanChoice === "rock" && computerChoice === "rock" ||
+            humanChoice === "scissors" && computerChoice === "scissors")
+            alert("That's a tie");
     }
 
-console.log(getComputerChoice()); 
-                                        // testing if these work
-console.log(getHumanChoice());
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(humanScore, computerScore);
 
+
+                                        // testing if these work
+
+    //console.log(getComputerChoice()); 
+    // console.log(getHumanChoice());
